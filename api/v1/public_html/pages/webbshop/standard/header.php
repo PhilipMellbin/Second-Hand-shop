8f93521555319d5f82bd0cea345bb55a33c933c4
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-06 20:53:20 +0100

    doing some minor cleanup. Continuing work on the header

--- api/v1/public_html/pages/webbshop/standard/header.php
+++ api/v1/public_html/pages/webbshop/standard/header.php
@@ -9,18 +9,26 @@
 <body>
    <header class="top">
     <div class="Title">
-        <h1 class="end">Second Academia</h1>
-        <div class="img"><img src="/Second_Academia_Shop/Second-Hand-shop/api/v1/public_html/img/column-line-black-icon-png_309179.png" alt=""> </div>
-        <h1>SecondHand equipment</h1>
+        <div class="pillar"><img src="https://cdn-icons-png.flaticon.com/512/4793/4793297.png" alt="LOGO"></div>
+        <div class="Logo">
+            <div class="logo"><img src="/Second-Hand-shop/api/v1/public_html/img/182956.png" alt=""></div>
+            <h1>Second Academia</h1>
+            <h2>Second hand study equipment</h2>
+        </div>
+        <div class="pillar"><img src="https://cdn-icons-png.flaticon.com/512/4793/4793297.png" alt="LOGO"></div>
+        <!--<h1 class="end">Second Academia</h1>-->
+        <!--<h1>SecondHand equipment</h1>-->
     </div>
     <nav>
-        <a class="link" href="index.php?page=home">home</a>
-        <p class="link" onmouseover="//display categories"></p>
+        <a class="Button" href="index.php?page=home">home</a>
+        <p class="Button" onmouseover="//display categories">categories</p>
         <form method="get" action="index.php?search=<?$inp?>">
             <label for="">Search...</label>
             <input type="text" name=<?$inp?>>
             <input type="submit" value="submit">
         </form>
+        <p class="Button"> select region</p>
+        <a href="index.php?page=login"></a>
     </nav>
    </header> 
 <main>
