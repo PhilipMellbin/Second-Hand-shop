0ff28429fb035d5c8cd38007e6a3c7a07b6a66af
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-06 11:26:19 +0100

    working on the header

--- api/v1/public_html/pages/webbshop/standard/header.php
+++ api/v1/public_html/pages/webbshop/standard/header.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/Second_Academia_Shop/Second-Hand-shop/api/v1/public_html/styles/style.css">
 </head>
 <body>
-   <header>
+   <header class="top">
     <div class="Title">
-        <h1>Second Academia</h1>
-        <img src="" alt=""> 
-        <h1>Second Hand equipment</h1>
+        <h1 class="end">Second Academia</h1>
+        <div class="img"><img src="/Second_Academia_Shop/Second-Hand-shop/api/v1/public_html/img/column-line-black-icon-png_309179.png" alt=""> </div>
+        <h1>SecondHand equipment</h1>
     </div>
     <nav>
         <a class="link" href="index.php?page=home">home</a>
-        <p onmouseover="//display categories"></p>
+        <p class="link" onmouseover="//display categories"></p>
         <form method="get" action="index.php?search=<?$inp?>">
             <label for="">Search...</label>
             <input type="text" name=<?$inp?>>
