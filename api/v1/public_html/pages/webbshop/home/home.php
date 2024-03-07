a283ae47ecb865a5ee55751af8e2f0884384c532
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-07 15:34:29 +0100

    working on slideshow

--- api/v1/public_html/pages/webbshop/home/home.php
+++ api/v1/public_html/pages/webbshop/home/home.php
@@ -1,11 +1,29 @@
 <?php //header ?>
 <section class="home">
     <button onclick="//forward();"><img src="" alt=""></button>
-    <div id="img_1"><img src="" alt=""></div>
-    <div id="img_2"><img src="" alt=""></div>
-    <div id="img_3"><img src="" alt=""></div>
+    <section class="slide" id="sec_1">
+        <div>
+            <h1>Woried about student dept?</h1>
+            <h2>You are in the right place!</h2>
+            <a href="#products">What do we have?</a>
+        </div>
+    </section>
+    <section class="slide" id="sec_2">
+        <div>
+            <h1>Best store for cheap study material</h1>
+            <h2>At least acording to xxx users</h2>
+            <a href="#products">What do we have?</a>
+        </div>
+    </section>
+    <section class="slide" id="sec_3">
+        <div>
+            <h1>Strugling with student dept?</h1>
+            <h2>Make an account now and sell your old equipment</h2>
+            <a href="index.php?page=login">Sign up now!</a>
+        </div>
+    </section>
     <button onclick="//backward();"><img src="" alt=""></button>
 </section>
-<section class="products">
+<section id="products" class="products">
     <h2>Products you may be intresed in</h2>
 <?php //products ?>
