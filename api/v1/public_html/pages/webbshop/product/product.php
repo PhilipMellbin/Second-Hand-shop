c3effc35d0137406e52f2be1a3438ee6e65a2a3c
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-19 12:46:10 +0100

    Adding new style to product page. may also try to implement some classes from home in order to make adding styles way easyer

--- api/v1/public_html/pages/webbshop/product/product.php
+++ api/v1/public_html/pages/webbshop/product/product.php
@@ -3,18 +3,22 @@
 </section>
 <div class="productmain">
     <section class="images">
-        <img src="<?=$this->img?>" alt="picture of <?=$this->title?>">
+        <div class="img">
+            <img src="<?=$this->img?>" alt="picture of <?=$this->title?>">
+        </div>
     </section>
     <section class="content">
         <div class="title">
         <h2><?=$this->title?></h2>
+        <div class="line first"></div>
+        <div class="line second"></div>
         </div>
         <div class="descriptions">
             <section>
-                <?=$this->description?>
+                <p><?=$this->description?></p>
             </section>
             <section>
-                <p>Publisher: <?=$this->publisher?></p>
+                <a href="index.php?page=account&user=<?=$this->publisher?>">published by <?=$this->publisher?></a>
                 <p>Publish date: <?=$this->publish_date?></p>
                 <h3><?=$this->price?> SEK</h3>
                 <form method="post" action="index.php?page=product&prod_id=<?=$this->prod_id?>">
