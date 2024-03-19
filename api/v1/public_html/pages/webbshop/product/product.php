d5cdabbbd137ede7ba31b2dd110b2c4594fe101d
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-19 09:16:44 +0100

    now i till try a diferent aproach

--- api/v1/public_html/pages/webbshop/product/product.php
+++ api/v1/public_html/pages/webbshop/product/product.php
@@ -2,23 +2,25 @@
     
 </section>
 <div class="productmain">
-    <h2><?=$this->title?></h2>
-    <div>
-        <section>
-            <div>
-                <img src="<?=$this->img?>" alt="picture of <?=$this->title?>">
-            </div>
-        </section>
-        <section>
-            <p>Publisher: <?=$this->publisher?></p>
-            <p>Publish date: <?=$this->publish_date?></p>
-            <p>Description:
+    <section class="images">
+        <img src="<?=$this->img?>" alt="picture of <?=$this->title?>">
+    </section>
+    <section class="content">
+        <div class="title">
+        <h2><?=$this->title?></h2>
+        </div>
+        <div class="descriptions">
+            <section>
                 <?=$this->description?>
-            </p>
-            <h3><?=$this->price?> SEK</h3>
-            <form method="post" action="index.php?page=product&prod_id=<?=$this->prod_id?>">
-                <button type="submit" name="action" value="add_cart">Ad to cart!</button>
-            </form>
-        </section>
-    </div>
+            </section>
+            <section>
+                <p>Publisher: <?=$this->publisher?></p>
+                <p>Publish date: <?=$this->publish_date?></p>
+                <h3><?=$this->price?> SEK</h3>
+                <form method="post" action="index.php?page=product&prod_id=<?=$this->prod_id?>">
+                    <button type="submit" name="action" value="add_cart">Ad to cart!</button>
+                </form>
+            </section>
+        </div>
+    </section>
 </div>
