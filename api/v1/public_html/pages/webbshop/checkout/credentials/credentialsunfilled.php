cc3e51531c46d0b3592381119679ea0dc60712be
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-21 11:54:23 +0100

    added all i could add to checkout. If i don't get swish to work, i will work on the cart now.

--- api/v1/public_html/pages/webbshop/checkout/credentials/credentialsunfilled.php
+++ api/v1/public_html/pages/webbshop/checkout/credentials/credentialsunfilled.php
@@ -1,6 +1,6 @@
 <article class="checkout_credentials unfilled">
     <header>
-    <img src="" alt="2">
+    <div><img src="/Second_Academia_shop/Second-Hand-shop/api/v1/public_html/img/svg/2.png" alt="2"></div>
         <h1>Credentials</h1>
     </header>
 </article>
