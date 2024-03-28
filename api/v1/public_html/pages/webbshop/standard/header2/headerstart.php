24e43ced42488c27b2bee68a90cb5c565efe8284
PhilipMellbin <gamer.mellbin@gmail.com>
2024-03-28 10:34:10 +0100

    everything semes strange with js and css. Will try to work on login now

--- api/v1/public_html/pages/webbshop/standard/header2/headerstart.php
+++ api/v1/public_html/pages/webbshop/standard/header2/headerstart.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/Second_Academia_Shop/Second-Hand-shop/api/v1/public_html/styles/style.css">
+    <link rel="stylesheet" href="/Second_Academia_Shop/Second-Hand-shop/api/v1/public_html/styles/stylee.css">
     <script src="/Second_Academia_Shop/Second-Hand-shop/api/v1/public_html/scripts/main.js"></script>
 </head>
 <body>
--- api/v1/public_html/scripts/main.js
+++ api/v1/public_html/scripts/main.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", function()
     const categories = document.getElementsByClassName("category");
 
     const header = document.getElementsByTagName("header");
+    console.log(header[0]);
+    console.log(header[0].style);
+    console.log(header[0].style.filter);
+    header[0].style.filter = "brightnes(20%)";
+    console.log(header[0].style.filter)
     const main = document.getElementsByTagName("main");
     const footer = document.getElementsByTagName("footer");
 
@@ -13,16 +18,17 @@ document.addEventListener("DOMContentLoaded", function()
     const cart_exit = document.getElementById("cart_exit");
     cart_btn.onclick = function()
     {
-        console.log(header[0].style.filter, main[0].style.filter, footer[0].style.filter);
+        header[0].style.filter = "brightnes(20%)";
+        main[0].style.filter ="brightnes(20%)";
+        footer[0].style.filter = "brightnes(20%)";
         cart[0].style.display = "block";
-        header[0].style.filter = main[0].style.filter = footer[0].style.filter = "brightnes(20%);";
         console.log(header[0].style.filter, main[0].style.filter, footer[0].style.filter);
     }
-    cat_btn.onclick = function()
+    /*cat_btn.onclick = function()
     {
         categories[0].style.display = "block";
         categories[0].style.filter="brightness(70%)"
-    }
+    }*/
     cart_exit.onclick = function()
     {
         cart[0].style.display = "none";
